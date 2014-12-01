6659ca08c58ef54f23a0437d116a4f6765d39be9
Chris Dziewa <chris.dziewa@gmail.com>
2014-12-01 09:29:07 -0600

    Finish second problem

---
+++ problem2.js
@@ -0,0 +1,19 @@
+function fibSequence(limit) {
+	var prev = 1;
+	var current = 1;
+	var fib = 0;
+	while (current < limit) {
+		if ((prev + current) % 2 == 0 
+			&& prev + current <= limit) {
+			fib += (prev + current);
+		}
+
+		old_current = current;
+		current = (prev + current);
+		prev = old_current;
+	}
+
+	return fib;
+}
+
+console.log(fibSequence(4000000));
